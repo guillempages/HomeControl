caf165661c00559717ff7eb8f3809f7633213ace
Guillem Pages <guillem@guillempages.cat>
2018-03-16 12:18:03 +0100

    Add missing javadoc to the annotation class

--- annotation-definition/src/main/java/cat/guillempages/homecontrol/annotations/HassEntity.java
+++ annotation-definition/src/main/java/cat/guillempages/homecontrol/annotations/HassEntity.java
@@ -7,15 +7,29 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by gupa8558 on 14.03.18.
+ * Classes annotated with this represent an HASS entity. An instance of every non-abstract class
+ * (or sub-class) with this annotation in its class hierarchy will be automatically added to the
+ * generated HassEntityMap class.
+ *
+ * The entity must have a public constructor accepting a "Hass" as a single argument.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 @Inherited
 public @interface HassEntity {
+
+    /**
+     * Entity classes. The values in this enum must exactly correspond to the basic abstract entity
+     * classes; the name of the types will be used as a type to generate the entity class
+     * collections.
+     */
     enum EntityType {
         Entity, Light, Remote, MediaPlayer;
     }
 
+    /**
+     * The type of entity this annotation refers to.
+     * @return The entity class.
+     */
     EntityType value() default EntityType.Entity;
 }
